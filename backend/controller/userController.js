29b4b6c60917396c8a29fdb6e2fb7ca491e86a9c
pankaj7073 <pankajvaishnav128@gmail.com>
2023-11-15 16:15:21 +0530

    changes

--- backend/controller/userController.js
+++ backend/controller/userController.js
@@ -1,26 +1,26 @@
 // const User = require("../models/User");
-// const nodemailer = require("nodemailer");
+const nodemailer = require("nodemailer");
 
 // Methods Has Been Start
-// const sendUser = async (req, res) => {
-//   const { name, email, subject, message } = req.body;
-//   try {
-//     const user = await User.create({
-//       name: name,
-//       email: email,
-//       subject: subject,
-//       message: message,
-//     });
-//     res.json({
-//       name: user.name,
-//       email: user.email,
-//       subject: user.subject,
-//       message: user.message,
-//     });
-//   } catch (err) {
-//     res.status(404).send("Do Not Send Data", err);
-//   }
-// };
+const sendUser = async (req, res) => {
+  const { name, email, subject, message } = req.body;
+  try {
+    const user = await User.create({
+      name: name,
+      email: email,
+      subject: subject,
+      message: message,
+    });
+    res.json({
+      name: user.name,
+      email: user.email,
+      subject: user.subject,
+      message: user.message,
+    });
+  } catch (err) {
+    res.status(404).send("Do Not Send Data", err);
+  }
+};
 
 // const sendUser = async (req, res) => {
 //   let { name, email, subject, message } = req.body;
@@ -58,4 +58,4 @@
 // };
 
 // Export Method
-// module.exports = sendUser;
+module.exports = sendUser;
