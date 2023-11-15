de48e6cbbca72437b232a9b3240fb298203c0b53
pankaj7073 <pankajvaishnav128@gmail.com>
2023-11-15 16:24:16 +0530

    changes

--- backend/controller/userController.js
+++ backend/controller/userController.js
@@ -2,60 +2,60 @@
 const nodemailer = require("nodemailer");
 
 // Methods Has Been Start
-const sendUser = async (req, res) => {
-  const { name, email, subject, message } = req.body;
-  try {
-    const user = await User.create({
-      name: name,
-      email: email,
-      subject: subject,
-      message: message,
-    });
-    res.json({
-      name: user.name,
-      email: user.email,
-      subject: user.subject,
-      message: user.message,
-    });
-  } catch (err) {
-    res.status(404).send("Do Not Send Data", err);
-  }
-};
-
 // const sendUser = async (req, res) => {
-//   let { name, email, subject, message } = req.body;
-//   console.log(name, email, subject, message);
+//   const { name, email, subject, message } = req.body;
 //   try {
-//     var transporter = nodemailer.createTransport({
-//       service: "gmail",
-//       auth: {
-//         user: process.env.EMAIL,
-//         pass: process.env.PASSWORD,
-//       },
-//     });
-
-//     var mailOptions = {
-//       from: email,
-//       to: process.env.EMAIL,
+//     const user = await User.create({
+//       name: name,
+//       email: email,
 //       subject: subject,
-//       text: message,
-//     };
-
-//     transporter.sendMail(mailOptions, function (error, info) {
-//       if (error) {
-//         console.log("not sending properly", error);
-//         res.status(500).send("Error sending email");
-//       } else {
-//         res.send("Send Mail Successfully");
-//         console.log("Email sent: " + info.response);
-//         // do something useful
-//       }
+//       message: message,
+//     });
+//     res.json({
+//       name: user.name,
+//       email: user.email,
+//       subject: user.subject,
+//       message: user.message,
 //     });
 //   } catch (err) {
-//     console.log("backend catch error", err);
-//     res.status(500).send("Error");
+//     res.status(404).send("Do Not Send Data", err);
 //   }
 // };
+
+const sendUser = async (req, res) => {
+  let { name, email, subject, message } = req.body;
+  console.log(name, email, subject, message);
+  try {
+    var transporter = nodemailer.createTransport({
+      service: "gmail",
+      auth: {
+        user: process.env.EMAIL,
+        pass: process.env.PASSWORD,
+      },
+    });
+
+    var mailOptions = {
+      from: email,
+      to: process.env.EMAIL,
+      subject: subject,
+      text: message,
+    };
+
+    transporter.sendMail(mailOptions, function (error, info) {
+      if (error) {
+        console.log("not sending properly", error);
+        res.status(500).send("Error sending email");
+      } else {
+        res.send("Send Mail Successfully");
+        console.log("Email sent: " + info.response);
+        // do something useful
+      }
+    });
+  } catch (err) {
+    console.log("backend catch error", err);
+    res.status(500).send("Error");
+  }
+};
 
 // Export Method
 module.exports = sendUser;
